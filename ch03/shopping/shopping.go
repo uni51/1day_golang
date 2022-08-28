fc4a33128d1657980f18ee289262d205ddcc7e8d
Koichi.Shimomura <unieye51@gmail.com>
2022-08-28 12:04:20 +0900

    3-4 scores.go

--- ch03/shopping/shopping.go
+++ ch03/shopping/shopping.go
@@ -4,9 +4,12 @@ import "fmt"
 
 func main() {
 	prices := []int{98, 125, 232, 147, 486}
+	items := []string{"消しゴム", "ボールペン", "ノート", "付箋紙", "バインダー"}
+	sum := 0
 
 	for i := 0; i < len(prices); i++ {
-
-		fmt.Printf("%d円\n", prices[i])
+		sum += prices[i]
+		fmt.Printf("%s: %d円\n", items[i], prices[i])
 	}
+	fmt.Printf("総計: %d円\n", sum)
 }
